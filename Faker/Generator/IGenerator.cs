e5b2efb8c69964a6945ee66b3f3b60878e5f3119
Suslenkova-Kseniya <92431229+Suslenkova-Kseniya@users.noreply.github.com>
2022-10-25 14:52:55 +0300

    Adding main generator interface

---
+++ Faker/Generator/IGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faker.Generator
+{
+    public interface IGenerator
+    {
+        object Generate();
+        Type GetGeneratorType();
+    }
+}
